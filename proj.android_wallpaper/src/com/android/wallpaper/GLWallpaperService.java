0e6c49d4b8708a654b5b4e2e00ff03c8c19c294a
Uliori <khallouki.elmehdi@gmail.com>
2016-01-11 01:24:01 +0000

    Commit N 10
    Added Android wallpaper template

---
+++ proj.android_wallpaper/src/com/android/wallpaper/GLWallpaperService.java
@@ -0,0 +1,155 @@
+package com.android.wallpaper;
+
+//import java.util.Timer;
+//import java.util.TimerTask;
+//import android.os.Handler;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Point;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLSurfaceView.Renderer;
+
+import android.preference.PreferenceManager;
+import android.service.wallpaper.WallpaperService;
+import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.WindowManager;
+
+//import com.google.android.gms.ads.AdListener;
+//import com.google.android.gms.ads.AdRequest;
+//import com.google.android.gms.ads.InterstitialAd;
+
+public abstract class GLWallpaperService extends WallpaperService {
+	
+	public static int width;
+	public static int height;
+//	private static InterstitialAd interstitial;
+//	private static Timer timer;
+//	Handler handler;
+	
+	//Preferences Stuffs
+
+	public static GLWallpaperService sharedS;
+	public static SharedPreferences getPreferences()
+	{
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(sharedS);
+		
+		// Créez la demande d'annonce.
+//		if (showads) {
+//		    AdRequest adRequest = new AdRequest.Builder().addTestDevice("46050842480CEB09AE1819558FF42F79").build();
+//		    interstitial.setAdListener(new AdListener() {});
+//		    interstitial.loadAd(adRequest);			
+//		}
+		return prefs;
+	}
+	
+//	public void displayInterstitial() {
+//		Log.e("Ads", "Display Method");
+//	    if (interstitial.isLoaded() && showads) {
+//	      interstitial.show();
+//	    }
+//	 }
+	
+//	private void runOnUiThread(Runnable runnable) {
+//        handler.post(runnable);
+//    }
+	
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		sharedS = this;
+//		handler = new Handler();
+//		// Créez l'interstitiel.
+//	    interstitial = new InterstitialAd(this);
+//	    interstitial.setAdUnitId("ca-app-pub-1060148675448438/5428665900");
+//	    timer = new Timer();
+//	    timer.scheduleAtFixedRate(new TimerTask() {
+//			
+//			@Override
+//			public void run() {
+//				// TODO Auto-generated method stub
+//				runOnUiThread(new Runnable() {
+//					
+//					@Override
+//					public void run() {
+//						// TODO Auto-generated method stub
+//						displayInterstitial();
+//					}
+//				});
+//			}
+//		}, 10000, 60000);
+	    
+	}
+	public class GLEngine extends Engine {
+		
+//		private static final String TAG = "GLEngine";
+		 
+		private WallpaperGLSurfaceView glSurfaceView;
+		private boolean rendererHasBeenSet;
+		 
+		@Override
+		public void onCreate(SurfaceHolder surfaceHolder) {
+		    super.onCreate(surfaceHolder);
+		    glSurfaceView = new WallpaperGLSurfaceView(GLWallpaperService.this);
+		}
+		
+		@Override
+		public void onDestroy() {
+		    super.onDestroy();
+		    glSurfaceView.onDestroy();
+		}
+		
+		protected void setRenderer(Renderer renderer) {
+		    glSurfaceView.setRenderer(renderer);
+		    rendererHasBeenSet = true;
+		}
+		 
+		protected void setEGLContextClientVersion(int version) {
+		    glSurfaceView.setEGLContextClientVersion(version);
+		}
+		 
+		protected void setPreserveEGLContextOnPause(boolean preserve) {
+		    glSurfaceView.setPreserveEGLContextOnPause(preserve);
+		}
+		@Override
+		public void onVisibilityChanged(boolean visible) {
+		    super.onVisibilityChanged(visible);
+		 
+		    if (rendererHasBeenSet) {
+		        if (visible) {
+		            glSurfaceView.onResume();
+		            GLWallpaperService.getPreferences();
+		        } else {
+		            glSurfaceView.onPause();            
+		        }
+		    }
+		}
+		
+		class WallpaperGLSurfaceView extends GLSurfaceView {
+//		    private static final String TAG = "WallpaperGLSurfaceView";
+		 
+		    WallpaperGLSurfaceView(Context context) {
+		        super(context);
+		    	WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		    	Display display = wm.getDefaultDisplay();
+			    Point size = new Point();
+			    display.getSize(size);
+			    GLWallpaperService.width = size.x;
+			    GLWallpaperService.height = size.y;
+		    }
+		 
+		    @Override
+		    public SurfaceHolder getHolder() {
+		        return getSurfaceHolder();
+		    }
+		 
+		    public void onDestroy() {
+		        super.onDetachedFromWindow();
+		    }
+		}
+		
+	}
+	
+}
