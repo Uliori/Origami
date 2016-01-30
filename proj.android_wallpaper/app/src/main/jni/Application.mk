5e554cf479c363001c2f06daf7e17c204b1a3419
Uliori <khallouki.elmehdi@gmail.com>
2016-01-30 15:10:48 +0000

    Commit N 17

---
+++ proj.android_wallpaper/app/src/main/jni/Application.mk
@@ -0,0 +1,6 @@
+APP_STL                 := gnustl_static
+APP_ABI                 := x86 armeabi-v7a
+APP_PLATFORM            := android-14
+APP_MODULES             := origami
+NDK_TOOLCHAIN_VERSION   := 4.8
+#APP_ABI := all
